f6e9144c305edbb49ae3c5056f52021dbfdebf1d
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2024-10-24 14:44:41 +0800

    Add files via upload

---
+++ add_test.R
@@ -0,0 +1,12 @@
+#' @Title Add Two Numbers
+#' @description
+#' This is a simple description
+#'
+#' @param x a number
+#' @param y another number
+#'
+#' @return sum of the two number
+#' @export
+#'
+#' @examples add(1,2)
+add_test <- function(x,y){x+y}
